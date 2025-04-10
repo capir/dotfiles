9fe4bd2402de10215a3bc8c4e8832a679ba2e225
Catalin Pirvu <catalin.pirvu@gmail.com>
2025-04-10 19:57:28 +0300

    nvim - disable arraw nav

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,3 +17,19 @@ vim.keymap.set("x", "p", '"_dP', { desc = "Paste yanked", silent = true })
 vim.keymap.set("i", ";;", "<Esc>A;<Esc>", { desc = "Append ; EOL", silent = true })
 vim.keymap.set("i", ",,", "<Esc>A,<Esc>", { desc = "Append . EOL", silent = true })
 vim.keymap.set("n", "<Leader>,", ":nohl<CR>", { desc = "Clear search highlight", silent = true })
+
+-- Disable arrow keys in Normal, Insert, and Visual modes
+vim.api.nvim_set_keymap("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("v", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Right>", "<Nop>", { noremap = true, silent = true })
